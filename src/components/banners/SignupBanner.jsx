f008aabdbd9d7f06ad19d311c31cc150deb8d67e
Jordan Vera <verawebdevelopment@gmail.com>
2024-08-13 19:32:47 -0500

    feat: Update SignupBanner component with improved UI and sign up button

--- src/components/banners/SignupBanner.jsx
+++ src/components/banners/SignupBanner.jsx
@@ -1,30 +1,20 @@
-import { useUser } from '@/providers/UserContext';
+import { useState } from 'react';
 import Link from 'next/link';
-// import { X } from 'lucide-solid';
-import {Close}
 
 const SignupBanner = () => {
-  const { user } = useUser();
-
   return (
-    <header className="multi_gradient_bg  fixed bottom-0 w-full px-5 py-2 flex items-center justify-between">
-      <h2 className="text-2xl text-black font-bold">M-M</h2>
-      <p className="text-black font-bold capitalize">
-        Track health progess by signing up now.{' '}
-        <span className="font-normal">The best nutrition app out.</span>
+    <footer className="fixed bottom-0 multi_gradient_bg w-full px-5 py-2 flex justify-between items-center">
+      <h2 className="text-2xl font-bold text-black">M-M</h2>
+
+      <p className="text-black font-bold text-md">
+        Track health progress and meals by signing up today.{' '}
+        <span className="font-normal">The best fitness app out there.</span>
       </p>
 
-      <Link
-        href="/?signup=true"
-        className="bg-black text-white px-3 py-1 rounded-md"
-      >
-        Signup
-      </Link>
-{/* 
-      <button>
-        <X />
-      </button> */}
-    </header>
+      <button className="bg-black text-white px-4 py-2 rounded-lg">
+        Sign Up
+      </button>
+    </footer>
   );
 };
 export default SignupBanner;
